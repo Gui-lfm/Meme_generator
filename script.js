0fb8f1fee35e408e8e6ff40aebd0e2797088b6b4
Guilherme Lucena <guilherme.lucena17@gmail.com>
2022-10-10 20:31:28 -0300

    requisitos 2,4 e 5 concluidos

--- script.js
+++ script.js
@@ -2,7 +2,18 @@
 
 const inputText = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
+const imgFile = document.getElementById('meme-insert');
+const imgPreview = document.getElementById('meme-image');
 
-inputText.addEventListener('input', function(event) {
-    memeText.innerText = event.target.value;
-})
+// requisito 1
+inputText.addEventListener('input', function (event) {
+  memeText.innerText = event.target.value;
+});
+
+// requisito 2
+imgFile.addEventListener('change', function (event) {
+  const img = event.target.files[0];
+  if (img) {
+    imgPreview.src = URL.createObjectURL(img);
+  }
+});
