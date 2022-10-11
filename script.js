2aea1817011430edde2afc84937317cdd9f75663
Guilherme Lucena <guilherme.lucena17@gmail.com>
2022-10-11 20:19:35 -0300

    requisitos 3 e 6 concluidos

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 // variaveis globais
-
 const inputText = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
 const imgFile = document.getElementById('meme-insert');
 const imgPreview = document.getElementById('meme-image');
+const btnFire = document.getElementById('fire');
+const btnWater = document.getElementById('water');
+const btnEarth = document.getElementById('earth');
+const memeContainer = document.getElementById('meme-image-container');
 
 // requisito 1
 inputText.addEventListener('input', function (event) {
@@ -16,4 +19,17 @@ imgFile.addEventListener('change', function (event) {
   if (img) {
     imgPreview.src = URL.createObjectURL(img);
   }
+});
+
+// requisito 6
+btnFire.addEventListener('click', function () {
+  memeContainer.style.border = '3px dashed red';
+});
+
+btnWater.addEventListener('click', function () {
+  memeContainer.style.border = '5px double blue';
+});
+
+btnEarth.addEventListener('click', function () {
+  memeContainer.style.border = '6px groove green';
 });
